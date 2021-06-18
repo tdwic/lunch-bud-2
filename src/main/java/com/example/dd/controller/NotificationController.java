6fbe749cc26b95a52a178f1c77acc7d0e1ca77ed
tdwic <wickramanayakatharindu@gmail.com>
2021-06-18 22:20:15 +0530

    sms configs

---
+++ src/main/java/com/example/dd/controller/NotificationController.java
@@ -0,0 +1,25 @@
+package com.example.dd.controller;
+
+import com.example.dd.services.Service;
+import com.example.dd.services.twilioService.SmsRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/sms")
+public class NotificationController {
+    private final Service service;
+
+    @Autowired
+    public NotificationController(Service service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void sendSms(@RequestBody SmsRequest smsRequest) {
+        service.sendSms(smsRequest);
+    }
+}
